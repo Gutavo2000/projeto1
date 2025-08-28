8ccd3932d7ebb12d2f33e25867c14f61ab37d6df
Gustavo Gabriel <gustavogabriel2000@gmail.com>
2025-08-28 11:34:53 -0300

    terceira alteração

--- meu_script.py
+++ meu_script.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
+minha_atividade_favorita = "Codar"
+
 print("Meu nome:", "Sofia")
 print("Minha cor favorita:", "Verde")
+print("Minha atividade favorita:", minha_atividade_favorita)
