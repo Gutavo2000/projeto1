130a20b194581b83c9a3127429d468bc2c0141ab
Gustavo Gabriel <gustavogabriel2000@gmail.com>
2025-08-28 11:40:10 -0300

    quinta alteração lista 1 python

--- meu_script.py
+++ meu_script.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-minha_atividade_favorita = "Codar"
+import sys
 
-print("Meu nome:", "Sofia")
-print("Minha cor favorita:", "Verde")
-print("Minha atividade favorita:", minha_atividade_favorita)
-print("Meu animal favorito:", "Galinha")
+meu_nome = sys.argv[1]
+cor_favorita = sys.argv[2]
+atividade_favorita = sys.argv[3]
+animal_favorito = sys.argv[4]
+
+print("Meu nome: " + meu_nome + " | Minha cor favorita: " + cor_favorita + " | Minha atividade favorita: " + atividade_favorita + " | Meu animal favorito: " + animal_favorito)
