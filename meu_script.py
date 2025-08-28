4eb705eb99efb5d5e168707bafce06d589170eb6
Gustavo Gabriel <gustavogabriel2000@gmail.com>
2025-08-28 11:37:33 -0300

    quarta alteração

--- meu_script.py
+++ meu_script.py
@@ -5,3 +5,4 @@ minha_atividade_favorita = "Codar"
 print("Meu nome:", "Sofia")
 print("Minha cor favorita:", "Verde")
 print("Minha atividade favorita:", minha_atividade_favorita)
+print("Meu animal favorito:", "Galinha")
